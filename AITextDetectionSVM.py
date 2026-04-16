9cf419d5b58e4418753f1e182131a6206e60385f
kcossboon <kc1046285@wcupa.edu>
2026-04-16 16:58:34 -0400

    Create AITextDetectionSVM.py

---
+++ AITextDetectionSVM.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
+
+df = pd.read_csv('AI_Human.csv')
+
+X = df["text"]        # input text
+y = df["generated"]   # labels (0 = human, 1 = AI)
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+
+vectorizer = TfidfVectorizer(ngram_range=(1,2), max_features=5000)
+
+X_train_tfidf = vectorizer.fit_transform(X_train)
+X_test_tfidf = vectorizer.transform(X_test)
+
+model = LinearSVC()
+model.fit(X_train_tfidf, y_train)
+
+y_pred = model.predict(X_test_tfidf)
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+print("Precision:", precision_score(y_test, y_pred, average='weighted'))
+print("Recall:", recall_score(y_test, y_pred, average='weighted'))
+print("F1:", f1_score(y_test, y_pred, average='weighted'))
